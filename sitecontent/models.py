d9ad04243578bb4e46eff422108898b51e22202a
Ashrith <ashrith@lostinkaos.com>
2014-12-04 13:02:26 +0530

    slug auto generate and removed character limit from content field

--- sitecontent/models.py
+++ sitecontent/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Content(models.Model):
   title = models.CharField(max_length=200)
-  content = models.TextField(max_length=1000)
-  slug = models.SlugField(max_length=40)
+  content = models.TextField()
+  slug = models.SlugField(null=True, blank=True)
+  def save(self, *args, **kwargs):
+    if not self.id:
+      self.slug = slugify(self.title)
+    super(Content, self).save(*args, **kwargs)
+
+  def __str__(self):
+        return self.title
